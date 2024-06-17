795226e3d5196eb497522db66b7135837d2fa98f
Gaboemi-Duoc <ga.jeriam@duocuc.cl>
2024-06-17 09:46:34 -0400

    Actualizacion 3 Valores

--- calculadora.py
+++ calculadora.py
@@ -1,29 +1,34 @@
 num1 = int(input("numero 1: ")) 
-num2 = int(input("numero 2: ")) 
+num2 = int(input("numero 2: "))
+num3 = int(input("numero 3: "))
 
 valor = 0
 while True:
-    print("""seleccione opcion
-            1- Sumar 
-            2- Restar
-            3- Multiplicar
-            4- dividir 
-        """)
+	print("""seleccione opcion
+			1- Sumar 
+			2- Restar
+			3- Multiplicar
+			4- Dividir 
+			5- Sumar 3 Numeros
+		""")
 
-    valor = int(input("Elige una opcion: ") )     
+	valor = int(input("Elige una opcion: ") )     
 
-    if valor == 1:
-        print("la suma es",num1+num2)
-        break;
-    if valor == 2:
-        print("la resta es",num1-num2)
-        break;
-    if valor == 3:
-        print("la multiplicacion es",num1*num2)
-        break;
-    if valor == 4:
-        print("la division es",num1/num2)
-        break;
-    else:
-        print("Opcion incorrecta")
-        break;
+	if valor == 1:
+		print("la suma es",num1+num2)
+		break;
+	if valor == 2:
+		print("la resta es",num1-num2)
+		break;
+	if valor == 3:
+		print("la multiplicacion es",num1*num2)
+		break;
+	if valor == 4:
+		print("la division es",num1/num2)
+		break;
+	if valor == 5:
+		print("la suma es",num1+num2+num3)
+		break;
+	else:
+		print("Opcion incorrecta")
+		break;
