d6e50d7d7d4d649fe89c4b8a321a00a34a74d8e2
Gaboemi-Duoc <ga.jeriam@duocuc.cl>
2024-06-17 10:08:15 -0400

    Updated Fun3 to something that works

--- Python/Fun 3.py
+++ Python/Fun 3.py
@@ -3,7 +3,8 @@ import json
 import requests
 system("cls")
 
-URL = "https://congresorest.appspot.com/"
+URL = "https://swapi.dev/"
 
 r = requests.get( url = URL )
 
+# Esto es como uno llama a una API en internet ;>
