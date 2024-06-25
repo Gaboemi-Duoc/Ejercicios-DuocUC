8a2210daa3b6f811c65d4183454a2e3367e121bf
Gaboemi-Duoc <ga.jeriam@duocuc.cl>
2024-06-25 12:48:39 -0400

    Create Ayudantia4.py

---
+++ Python/Ayudantia4.py
@@ -0,0 +1,21 @@
+from os import system
+from json import dump, load
+
+## Funciones ##
+def ingresarReservaHabitacion():
+	pass
+def consultarEstadoHabitacion():
+	pass
+def eliminarReservaHabitacion():
+	pass
+def archivoExportar():
+	pass
+
+## Programa ##
+habitaciones = []
+opc = -1
+while opc != 4:
+	if True:
+		pass
+	else:
+		print("Error!")
