eb03e1f9662bdeadd2b50c4032c7feeb9f87130b
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2025-03-19 12:45:24 -0300

    Clase 3 - Base de Datos II

--- Semestre 3/Base de Datos II/1.1.3 Actividad.sql
+++ Semestre 3/Base de Datos II/1.1.3 Actividad.sql
@@ -4,5 +4,5 @@ SELECT
     TO_CHAR(fecha_nac_cli,'DD/MM/YYYY') AS "FECHA NACIMIENTO"
 FROM CLIENTE
 WHERE EXTRACT(DAY FROM fecha_nac_cli)=20+1 AND EXTRACT(MONTH FROM fecha_nac_cli)=8
-/*WHERE '21/08' = TO_CHAR(fecha_nac_cli, 'DD/MM')*/
+--WHERE '21/08' = TO_CHAR(fecha_nac_cli, 'DD/MM')
 ORDER BY numrun_cli;
--- Semestre 3/Base de Datos II/1.2.2 Actividad.sql
+++ Semestre 3/Base de Datos II/1.2.2 Actividad.sql
@@ -5,7 +5,7 @@
 SELECT
     carreraid as "IDENTIFICACION DE LA CARRERA",
     COUNT(*) AS "TOTAL DE ALUMNOS MATRICULADOS",
-    'Le corresponden ' || TO_CHAR( COUNT(*) * &MONTO,'$999G999' ) || ' del presupuesto total asignado para publicidad' as "MONTO POR PUBLICIDAD"
+    'Le corresponden' || TO_CHAR( COUNT(*) * &MONTO,'$999G999' ) || ' del presupuesto total asignado para publicidad' as "MONTO POR PUBLICIDAD"
 FROM alumno
 GROUP BY carreraid
 ORDER BY COUNT(*) DESC, carreraid ASC;
@@ -18,13 +18,55 @@ SELECT
 FROM ALUMNO
 GROUP BY carreraid
 HAVING COUNT(*) > 4
-ORDER BY carreraid;
+ORDER BY carreraid; -- ASC BY DEFAULT
 
 -- CASO 3
 
 SELECT
-    run_jefe AS "RUN JEFE SIN DV",
+    TO_CHAR(run_jefe, '99g999g999') AS "RUN JEFE SIN DV",
     COUNT(*) AS "TOTAL DE EMPLEADOS A SU CARGO",
-    TO_CHAR( MAX(salario), '9G999G999' ) AS "SALARIO MAXIMO"
+    TO_CHAR( MAX(salario), '9G999G999' ) AS "SALARIO MAXIMO",
+    TO_CHAR(COUNT(*)*10, '990')||'% del Salario Máximo' AS "PORCENTAJE DE BONIFICACION",
+    TO_CHAR( MAX(salario) * ( COUNT(*)/10 ) , '$9G999G999' ) AS "BONIFICACION"
+FROM empleado
 WHERE run_jefe is not NULL
-GROUP BY run_jefe;
+GROUP BY run_jefe
+ORDER BY run_jefe; -- ASC BY DEFAULT
+
+-- CASO 4
+
+SELECT
+    id_escolaridad AS "ESCOLARIDAD",
+    CASE
+        WHEN id_escolaridad = 10 THEN 'BÁSICA'
+        WHEN id_escolaridad = 20 THEN 'MEDIA CIENTÍFICA HUMANISTA'
+        WHEN id_escolaridad = 30 THEN 'MEDIA TÉCNICO PROFESIONAL'
+        WHEN id_escolaridad = 40 THEN 'SUPERIOR CENTRO DE FORMACIÓN TÉCNICA'
+        WHEN id_escolaridad = 50 THEN 'SUPERIOR INSTITUTO PROFESIONAL'
+        WHEN id_escolaridad = 60 THEN 'SUPERIOR UNIVERSIDAD'
+    END AS "DESCRIPCIÓN ESCOLARIDAD",
+    COUNT(*) AS "TOTAL DE EMPLEADOS",
+    TO_CHAR(MAX(salario),'$99G999G999') AS "SALARIO MAXIMO",
+    TO_CHAR(MIN(salario),'$99G999G999') AS "SALARIO MINIMO",
+    TO_CHAR(SUM(salario),'$99G999G999') AS "SALARIO TOTAL",
+    TO_CHAR(ROUND(AVG(salario)),'$99G999G999') AS "SALARIO PROMEDIO",
+    TO_CHAR(MEDIAN(salario),'$99G999G999') AS "SALARIO MEDIANO"
+FROM EMPLEADO
+GROUP BY id_escolaridad
+ORDER BY 3 DESC; -- ORDENAR POR LA COLUMNA 3
+
+-- CASO 5
+-- CURRENT YEAR = 2025
+SELECT
+    tituloid AS "CODIGO DEL LIBRO",
+    COUNT(*) AS "TOTAL DE VECES SOLICITADO",
+    CASE
+        WHEN COUNT(*) > 5 THEN 'Se requiere comprar 4 nuevos ejemplares'
+        WHEN COUNT(*) BETWEEN 4 AND 5 THEN 'Se requiere comprar 2 nuevos ejemplares'
+        WHEN COUNT(*) BETWEEN 2 AND 3 THEN 'Se requiere comprar 1 nuevos ejemplares'
+        WHEN COUNT(*) = 1 THEN 'No se requiere nuevos ejemplares'
+    END AS "SUGERENCIA"
+FROM prestamo
+WHERE '24' = TO_CHAR(fecha_ini_prestamo, 'YY')
+GROUP BY tituloid
+ORDER BY 2 DESC;
