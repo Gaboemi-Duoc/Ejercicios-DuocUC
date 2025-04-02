eca8f44b31f3bc5e9f6e27508fcf78d3eb178f0d
Gaboemi-Duoc <173064947+Gaboemi-Duoc@users.noreply.github.com>
2025-04-02 13:02:36 -0300

    Clase 7 - Base de Datos II

--- Semestre 3/Base de Datos II/1.4.2 Actividad.sql
+++ Semestre 3/Base de Datos II/1.4.2 Actividad.sql
@@ -29,4 +29,30 @@ GROUP BY
 ORDER BY SUM(trans.monto_transaccion), c.appaterno;
 
 --CASO 3
-SELECT * FROM transaccion_tarjeta_cliente NATURAL JOIN tipo_transaccion_tarjeta;
+SELECT
+    TO_CHAR(trans.fecha_transaccion, 'MMYYYY') AS "FECHA TRANSACCION",
+    tip.nombre_tptran_tarjeta AS "TIPO TRANSACCIÓN",
+    LPAD(TO_CHAR(SUM(trans.monto_total_transaccion), '$99G999G999'),26) AS "MONTO AVANCES/SUPER AVANCES",
+    LPAD(TO_CHAR(SUM(trans.monto_total_transaccion*(sbif.porc_aporte_sbif/100)), '$99G999'), 14) AS "APORTE A LA SBIF"
+FROM transaccion_tarjeta_cliente trans NATURAL JOIN tipo_transaccion_tarjeta tip
+    JOIN aporte_sbif sbif ON(trans.monto_total_transaccion BETWEEN sbif.monto_inf_av_sav AND sbif.monto_sup_av_sav )
+WHERE EXTRACT(YEAR FROM trans.fecha_transaccion) = EXTRACT(YEAR FROM SYSDATE) AND
+    cod_tptran_tarjeta IN (102, 103)
+GROUP BY
+    TO_CHAR(trans.fecha_transaccion, 'MMYYYY'),
+    tip.nombre_tptran_tarjeta
+ORDER BY 1,2;
+
+--CASO 4
+SELECT
+    TO_CHAR(c.numrun,'09G999G999') || '-' || c.dvrun AS "RUN CLIENTE",
+    c.pnombre || ' ' || c.snombre || ' ' || c.appaterno || ' ' || c.apmaterno AS "NOMBRE CLIENTE",
+    LPAD(TO_CHAR(NVL(SUM(trans.monto_transaccion),0),'$999G999G999'),24) AS "COMPRAS/AVANCES/S.AVANCES"
+FROM cliente c LEFT JOIN tarjeta_cliente tar
+    ON(c.numrun = tar.numrun)
+    LEFT JOIN transaccion_tarjeta_cliente trans
+    ON(tar.nro_tarjeta = trans.nro_tarjeta)
+GROUP BY
+    TO_CHAR(c.numrun,'09G999G999') || '-' || c.dvrun,
+    c.pnombre || ' ' || c.snombre || ' ' || c.appaterno || ' ' || c.apmaterno, c.appaterno
+ORDER BY c.appaterno, 3 DESC;
