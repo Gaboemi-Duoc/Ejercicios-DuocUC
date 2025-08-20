39eeb3c7829f3ef229d9d6f7823f70136c2fe6cc
PV-Alumno <173064947+Gaboemi-Duoc@users.noreply.github.com>
2025-08-20 12:08:59 -0400

    Clase 4 - Talle Base de Datos

--- Semestre 4/Taller Base de Datos/Clase 3.sql
+++ Semestre 4/Taller Base de Datos/Clase 3.sql
@@ -1,12 +1,11 @@
 DECLARE
     CURSOR cur_paises IS
-
-    SELECT
-      country_id,
-      country_name nombre_pais,
-      region_id
-    FROM
-      countries;
+        SELECT
+          country_id,
+          country_name nombre_pais,
+          region_id
+        FROM
+          countries;
 BEGIN
     -- Alternativa SIMPLE LOOP
     -- OPEN cur_paises;
@@ -22,5 +21,70 @@ BEGIN
         dbms_output.put_line(reg.nombre_pais);
     END LOOP;
 
-
 END;
+
+-- Actividad 1
+DECLARE
+    CURSOR c_employee_dep_minsal(p_dep_id NUMBER, p_min_sal NUMBER) IS
+        SELECT
+            first_name || ' ' || last_name nombre_empleado,
+            department_id dep_id,
+            salary salario
+        FROM employees
+        WHERE
+            department_id = p_dep_id AND
+            salary >= p_min_sal;
+BEGIN
+    FOR reg IN c_employee_dep_minsal(50, 5000) LOOP
+        dbms_output.put_line(reg.nombre_empleado);
+    END LOOP;
+END;
+
+-- Actividad 2
+DECLARE
+     CURSOR cur_employee_salaries IS
+        SELECT
+            first_name || ' ' || last_name nombre_empleado,
+            salary salario
+        FROM
+            employees;
+BEGIN
+    FOR reg IN cur_employee_salaries LOOP
+        dbms_output.put_line(reg.nombre_empleado || ' - Salario: ' || reg.salario);
+    END LOOP;
+END;
+
+-- Actividad 3
+DECLARE
+    CURSOR cur_countries IS
+        SELECT country_name, region_name
+        FROM countries NATURAL JOIN regions;
+BEGIN
+    FOR reg IN cur_countries LOOP
+        dbms_output.put_line('NAME COUNTRY: ' || reg.country_name || ' - REGION: ' || reg.region_name); 
+    END LOOP;
+END;
+
+
+
+-- Saved Procedure
+CREATE OR REPLACE PROCEDURE sp_paises(p_area1 NUMBER, p_area2 NUMBER) IS
+    CURSOR cur_paises(p_region NUMBER) IS
+        SELECT
+            country_id,
+            country_name nombre_pais,
+            region_id
+        FROM
+            countries
+        WHERE region_id = p_region;
+BEGIN
+    FOR reg in cur_paises(p_area1) LOOP
+        dbms_output.put_line(upper(reg.nombre_pais));
+    END LOOP;
+    dbms_output.put_line('');
+    FOR reg in cur_paises(p_area2) LOOP
+        dbms_output.put_line(upper(reg.nombre_pais));
+    END LOOP;
+END;
+-- Execution
+EXEC sp_paises(10,20);
